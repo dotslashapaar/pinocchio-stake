5346b9bc33836b6cdfc344fb06a9c50c2887aeaf
aseneca <princeosuh@gmail.com>
2025-05-01 11:54:28 +0100

    defined state for Authorize with seeds

---
+++ program/src/state/authorized_with_seed.rs
@@ -0,0 +1,9 @@
+use pinocchio::pubkey::Pubkey;
+
+#[repr(C)]
+#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
+pub struct AuthorizedWithSeeds {
+    pub stake_authorize: Pubkey,
+    pub authority_seed: Pubkey,
+    pub authority_owner:String,
+}
