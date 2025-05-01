6dff5f7cf992ab9a970e8beb500b8464d36b0a51
aseneca <princeosuh@gmail.com>
2025-05-01 11:56:58 +0100

    correction: state was defined for AuthorizedCheckedWithSeeds

--- program/src/state/authorized_with_seed.rs
+++ program/src/state/authorized_with_seed.rs
@@ -2,7 +2,7 @@ use pinocchio::pubkey::Pubkey;
 
 #[repr(C)]
 #[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
-pub struct AuthorizedWithSeeds {
+pub struct AuthorizedCheckedWithSeeds {
     pub stake_authorize: Pubkey,
     pub authority_seed: Pubkey,
     pub authority_owner:String,
