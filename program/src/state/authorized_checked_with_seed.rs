bd385d356e3585b77e62408764a14ccba81f2cce
aseneca <princeosuh@gmail.com>
2025-05-02 09:31:29 +0100

    defined stake auth enum. refactored code.

--- program/src/state/authorized_checked_with_seed.rs
+++ program/src/state/authorized_checked_with_seed.rs
@@ -1,9 +1,19 @@
+#![no_std]
+extern crate alloc;
+
+use alloc::string::String;
 use pinocchio::pubkey::Pubkey;
 
 #[repr(C)]
-#[derive(Default, Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum StakeAuthorize {
+    Staker,
+    Withdrawer,
+}
+#[repr(C)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct AuthorizeCheckedWithSeedArgs {
-    pub stake_authorize: Pubkey,
-    pub authority_seed: Pubkey,
-    pub authority_owner:String,
+    pub stake_authorize: StakeAuthorize,
+    pub authority_seed: String,
+    pub authority_owner:Pubkey,
 }
