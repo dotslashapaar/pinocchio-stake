9c46555e6881bc601a32b4eaa5436c085888ba41
aseneca <princeosuh@gmail.com>
2025-05-09 11:31:17 +0100

    modified struct to use &'a str for valid UTF-8 guarantees

--- program/src/state/authorized_checked_with_seed.rs
+++ program/src/state/authorized_checked_with_seed.rs
@@ -1,14 +1,12 @@
-#![no_std]
-extern crate alloc;
 
-use alloc::string::String;
 use pinocchio::pubkey::Pubkey;
 use crate::state::stake_authorize::StakeAuthorize;
 
 #[repr(C)]
 #[derive(Debug, PartialEq, Eq, Clone)]
-pub struct AuthorizeCheckedWithSeedArgs {
+pub struct AuthorizeCheckedWithSeedArgs <'a>{
     pub stake_authorize: StakeAuthorize,
-    pub authority_seed: String,
+    pub authority_seed_len:u32,
+    pub authority_seed: &'a str,
     pub authority_owner:Pubkey,
 }
